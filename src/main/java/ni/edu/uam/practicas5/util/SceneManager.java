b7b2446b9554b428f1a6a1237282cc1216694a51
Diego Chamendy <dmchamendy@uamv.edu.ni>
2026-09-16 14:46:18 -0600

    Menu Principal COntroller

--- src/main/java/ni/edu/uam/practicas5/util/SceneManager.java
+++ src/main/java/ni/edu/uam/practicas5/util/SceneManager.java
@@ -1,4 +1,24 @@
 package ni.edu.uam.practicas5.util;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SceneManager {
+    private SceneManager(){ }
+
+    public static void abrirVentana(String recurso, String titulo)throws IOException {
+        var url = SceneManager.class.getResource(recurso);
+        if (url == null) throw new IOException("Archivo FXML no encontrado" + recurso);
+
+        Stage stage = new Stage();
+        stage.setTitle(titulo);
+        stage.setScene(new Scene(new FXMLLoader(url).load()));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
 }
+
